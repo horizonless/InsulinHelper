f24d08dfe6291de28a2c6c733ac4fb44222cf322
Shenrui Zhang <zhangshenrui@coconutislandstudio.com>
2020-11-17 21:41:11 +0800

    Initial Commit

---
+++ InsulinHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InsulinHelper
+//
+//  Created by Shenrui Zhang on 11/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
